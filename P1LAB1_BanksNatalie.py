2a287c1de42cd9ba6c14cf644acc460038f23325
banksnatalie90 <banksn1219@student.faytechcc.edu>
2025-06-16 08:58:47 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_BanksNatalie.py
@@ -0,0 +1,9 @@
+# Natalie Banks
+# 06/14/2025
+# P1LAB1
+# Learning the basics of python
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello, " + first_name + " " + last_name + "! Welcome to CTI-110")
