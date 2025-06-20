6e1a23668f83a38f7a35b00a29e48ae375723990
banksnatalie90 <banksn1219@student.faytechcc.edu>
2025-06-19 21:18:37 -0400

    p2lab1
    mathematical functions

---
+++ P2LAB1_BanksNatalie.py
@@ -0,0 +1,22 @@
+# Natalie Banks
+# 6-20-2025
+# P2LAB1
+# Mathematical functions in python
+
+
+import math
+
+
+radius = float (input("What is the radius of the circle? "))
+
+
+
+diameter = 2 * radius
+circumference = 2 * math.pi * radius
+area = math.pi * (radius ** 2)
+
+
+
+print (f"The diameter of the circle is {diameter:.1f}")
+print (f"The circumference of the circle is {circumference:.2f}")
+print (f"The area of the circle is {area:.3f}")
