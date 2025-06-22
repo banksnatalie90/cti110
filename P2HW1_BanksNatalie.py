837145cdd0dcbda3b5baebf80cb2ab9a184edf85
banksnatalie90 <banksn1219@student.faytechcc.edu>
2025-06-22 18:16:28 -0400

    p2hw1
    format

---
+++ P2HW1_BanksNatalie.py
@@ -0,0 +1,14 @@
+Python 3.13.5 (v3.13.5:6cb20a219a8, Jun 11 2025, 12:23:45) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> 
+========== RESTART: /Users/beauadamson/Downloads/P1HW2_BanksNatalie.py =========
+Enter your budget: 800
+Enter your travel destination: jjjjj
+Enter amount you will spend on gas: 70
+Enter amount you will spend on accommodation: 90
+Enter the amount you will spend on food:70
+
+--- Travel Budget ---
+Destination:        jjjjj
+Total Expenses:      $230.00
+Remaing Budget:      $570.00
